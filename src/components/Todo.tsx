bd971f5c029a5645402beadd08666221f7a5cad4
R Tretyakov <tretyakov.rv@mail.ru>
2025-04-09 13:01:49 +1000

    Законнчил добавление новой задачи и отображение добавленных задач

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -8,13 +8,29 @@ interface Task {
   completed: boolean;
 }
 
+let tasks: Task[] = [];
+
 const Todo: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
+
+  // useEffect(() => { }, [tasks]);
+
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // Здесь добавить логику добавления задачи
+    tasks.push({
+      id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      text: inputValue,
+      completed: false
+    });
     setInputValue("");
+  };
+
+  const deleteTask = (e: React.MouseEvent<HTMLElement>) => {
+    let id = Number(e.currentTarget.dataset.id);
+    tasks = tasks.filter(item => item.id !== id);
+    console.log(tasks);
   };
 
   return (
@@ -37,8 +53,18 @@ const Todo: React.FC = () => {
         </button>
       </form>
 
-      <div className='tasks-list'>{/* Здесь должны отображаться задачи */}</div>
-    </div>
+      <div className='tasks-list'>{/* Здесь должны отображаться задачи */}
+        {tasks.map(item =>
+        (<li key={item.id} style={{ listStyleType: 'none' }}>
+          <input type='checkbox' checked={item.completed}></input>
+          <span style={{ textDecoration: item.completed ? 'line-through' : '' }}>
+            {item.text}
+          </span>
+          <button onClick={(e) => deleteTask(e)} data-id={item.id}>x</button>
+        </li>))}
+
+      </div>
+    </div >
   );
 };
 
